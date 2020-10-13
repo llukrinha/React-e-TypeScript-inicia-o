ca498560f4744b22b62c71df94e1f1d1c335caeb
llukrinha <https://github.com/llukrinha/Api-Rest-Spring-Boot.git>
2020-10-13 12:19:43 -0300

    React_TypeScript MenuLateral

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-
-import {Container} from "./styles";
+import logoImg from "../../assets/logo.svg"
+import {MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp} from "react-icons/md"
+import {Container, Header, LogImg, MenuContainer, MenuItemLink, Title} from "./styles";
 
 const Aside: React.FC = () => {
 
     return (
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogImg src={logoImg} alt="Logo Minha Carteira"/>
+                <Title>Minha Carteira</Title>
+            </Header>
+            <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard/>
+                    Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowUpward/>
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowDownward/>
+                    Saídas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdExitToApp/>
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
         </Container>
 
     );
