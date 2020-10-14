70d0543250dbd0148e9682dd7e3cb063d2e43424
lucas.cantanhede <lucascantanhende@seap.ma.gov.br>
2020-10-14 16:35:31 -0300

    React_TypeScript Lista de movimento financeiro

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ContentHeader from "../../components/ContentHeader";
-import {Container} from "./styles";
+import {Container, Content} from "./styles";
 import SelectInput from "../../components/SelectInput";
+import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 const List: React.FC = () => {
 
@@ -16,6 +17,46 @@ const List: React.FC = () => {
             <ContentHeader title="List" lineColor="#f7931b">
                 <SelectInput options={options}/>
             </ContentHeader>
+            <Content>
+                <HistoryFinanceCard cardColor={"#313862"}
+                                    tagColor={"#e44c4e"}
+                                    title={"Conta de Luz"}
+                                    subtitle={"27/07/2020"}
+                                    amount={"R$130,00"}
+                />
+            </Content>
+            <Content>
+                <HistoryFinanceCard cardColor={"#313862"}
+                                    tagColor={"#e44c4e"}
+                                    title={"Conta de Luz"}
+                                    subtitle={"27/07/2020"}
+                                    amount={"R$130,00"}
+                />
+            </Content>
+            <Content>
+                <HistoryFinanceCard cardColor={"#313862"}
+                                    tagColor={"#e44c4e"}
+                                    title={"Conta de Luz"}
+                                    subtitle={"27/07/2020"}
+                                    amount={"R$130,00"}
+                />
+            </Content>
+            <Content>
+                <HistoryFinanceCard cardColor={"#313862"}
+                                    tagColor={"#e44c4e"}
+                                    title={"Conta de Luz"}
+                                    subtitle={"27/07/2020"}
+                                    amount={"R$130,00"}
+                />
+            </Content>
+            <Content>
+                <HistoryFinanceCard cardColor={"#313862"}
+                                    tagColor={"#e44c4e"}
+                                    title={"Conta de Luz"}
+                                    subtitle={"27/07/2020"}
+                                    amount={"R$130,00"}
+                />
+            </Content>
         </Container>
     );
 }
