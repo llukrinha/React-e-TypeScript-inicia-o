79e87ab7a7da1a8a123c0732b0122519383cd7d4
lucas.cantanhede <lucascantanhende@seap.ma.gov.br>
2020-10-14 17:48:54 -0300

    React_TypeScript Criando Filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,57 +1,42 @@
 import React from "react";
 import ContentHeader from "../../components/ContentHeader";
-import {Container, Content} from "./styles";
+import {Container, Content, Filters} from "./styles";
 import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 const List: React.FC = () => {
 
-    const options = [
-        {value: "Rodrido", lable: "Rodrigo"},
-        {value: "Maria", lable: "Maria"},
-        {value: "Ana", lable: "Ana"},
+    const months = [
+        {value: 7, lable: "Julho"},
+        {value: 8, lable: "Agosto"},
+        {value: 9, lable: "Setembro"},
     ]
-
+    const years = [
+        {value: 2020, lable: 2020},
+        {value: 2019, lable: 2019},
+        {value: 2018, lable: 2018},
+    ]
     return (
         <Container>
             <ContentHeader title="List" lineColor="#f7931b">
-                <SelectInput options={options}/>
+                <SelectInput options={months}/>
+                <SelectInput options={years}/>
             </ContentHeader>
+
+            <Filters>
+                <button type="button"
+                        className="tag-filter tag-filter-recurrent"
+                >
+                    Recorrentes
+                </button>
+                <button type="button"
+                        className="tag-filter tag-filter-eventual"
+                >
+                    Eventuais
+                </button>
+            </Filters>
             <Content>
-                <HistoryFinanceCard cardColor={"#313862"}
-                                    tagColor={"#e44c4e"}
-                                    title={"Conta de Luz"}
-                                    subtitle={"27/07/2020"}
-                                    amount={"R$130,00"}
-                />
-            </Content>
-            <Content>
-                <HistoryFinanceCard cardColor={"#313862"}
-                                    tagColor={"#e44c4e"}
-                                    title={"Conta de Luz"}
-                                    subtitle={"27/07/2020"}
-                                    amount={"R$130,00"}
-                />
-            </Content>
-            <Content>
-                <HistoryFinanceCard cardColor={"#313862"}
-                                    tagColor={"#e44c4e"}
-                                    title={"Conta de Luz"}
-                                    subtitle={"27/07/2020"}
-                                    amount={"R$130,00"}
-                />
-            </Content>
-            <Content>
-                <HistoryFinanceCard cardColor={"#313862"}
-                                    tagColor={"#e44c4e"}
-                                    title={"Conta de Luz"}
-                                    subtitle={"27/07/2020"}
-                                    amount={"R$130,00"}
-                />
-            </Content>
-            <Content>
-                <HistoryFinanceCard cardColor={"#313862"}
-                                    tagColor={"#e44c4e"}
+                <HistoryFinanceCard tagColor={"#e44c4e"}
                                     title={"Conta de Luz"}
                                     subtitle={"27/07/2020"}
                                     amount={"R$130,00"}
