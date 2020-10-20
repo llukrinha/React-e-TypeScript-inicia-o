6dee3cd62e60ed069c80aca3993c8a5a95ee21cb
lucas.cantanhede <lucascantanhende@seap.ma.gov.br>
2020-10-20 15:09:11 -0300

    React_TypeScript Segunda Refatoração

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -31,8 +31,8 @@ interface IRouteParams {
 
 const List: React.FC<IRouteParams> = ({match}) => {
     const [data, setData] = useState<IData[]>([]);
-    const [monthSelected, setMonthSelected] = useState<string>(String(new Date().getMonth() + 1));
-    const [yearSelected, setYearSelected] = useState<string>(String(new Date().getFullYear()));
+    const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth() + 1);
+    const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
     const [frequencyFilterSelected, setFrequencyFilterSelected] = useState(["recorrente", "eventual"]);
 
     const movimentType = match.params.type;
@@ -91,13 +91,31 @@ const List: React.FC<IRouteParams> = ({match}) => {
         }
     }
 
+    const handleMonthSelected = (month: string) => {
+        try {
+            const parseMonth = Number(month);
+            setMonthSelected(parseMonth);
+        } catch (error) {
+            throw  new Error("invalid month value. is accept 0 - 24.")
+        }
+    }
+
+    const handleYearSelected = (year: string) => {
+        try {
+            const parseYear = Number(year);
+            setYearSelected(parseYear);
+        } catch (error) {
+            throw  new Error("invalid year value. Is accept integer numbers.")
+        }
+    }
+
     useEffect(() => {
         const {data} = pageData;
 
         const filteredData = data.filter(item => {
             const date = new Date(item.date);
-            const month = String(date.getMonth() + 1);
-            const year = String(date.getFullYear());
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
 
             return month === monthSelected && year === yearSelected && frequencyFilterSelected.includes(item.frequency);
         });
@@ -120,10 +138,10 @@ const List: React.FC<IRouteParams> = ({match}) => {
         <Container>
             <ContentHeader title={pageData.title} lineColor={pageData.liveColor}>
                 <SelectInput options={months}
-                             onChange={(e) => setMonthSelected(e.target.value)}
+                             onChange={(e) => handleMonthSelected(e.target.value)}
                              defaultValue={monthSelected}/>
                 <SelectInput options={years}
-                             onChange={(e) => setYearSelected(e.target.value)}
+                             onChange={(e) => handleYearSelected(e.target.value)}
                              defaultValue={yearSelected}/>
             </ContentHeader>
 
