ac24fcad54cd8ede65f65610b4e0e8924a3947e8
lucas.cantanhede <lucascantanhende@seap.ma.gov.br>
2020-10-29 11:31:28 -0300

    React_TypeScript Implementando tela de login

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 
+import logoImg from "../../assets/logo.svg"
+
+import {Container, Logo, Form, FormTitle} from "./styles";
+
 const Signin: React.FC = () => {
-    return(
-        <h1>Signin</h1>
+    return (
+        <Container>
+            <Logo>
+                <img src={logoImg} alt="Minha Carteira"/>
+                <h2>Minha Carteira</h2>
+            </Logo>
+            <Form>
+                <FormTitle>Entrar</FormTitle>
+                <input type="text"></input>
+                <input type="text"></input>
+
+                <button type="submit"> Acessar</button>
+            </Form>
+        </Container>
+
     );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 
-import App from "./routes";
+// import App from "./routes";
+import Auth from "./auth.routes";
 
 const Routes: React.FC = () => (
     <BrowserRouter>
-        <App/>
+        <Auth/>
     </BrowserRouter>
 )
 export default Routes;
