947caa6cf848e6c1643a444830cd2d0ce523ec9b
llukrinha <https://github.com/llukrinha/Api-Rest-Spring-Boot.git>
2020-10-30 21:01:49 -0300

    React_TypeScript Botão sair

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,33 +1,38 @@
 import React from "react";
 import logoImg from "../../assets/logo.svg"
 import {MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp} from "react-icons/md"
-import {Container, Header, LogImg, MenuContainer, MenuItemLink, Title} from "./styles";
 
-const Aside: React.FC = () => (
-    <Container>
-        <Header>
-            <LogImg src={logoImg} alt="Logo Minha Carteira"/>
-            <Title>Minha Carteira</Title>
-        </Header>
-        <MenuContainer>
-            <MenuItemLink href="/">
-                <MdDashboard/>
-                Dashboard
-            </MenuItemLink>
-            <MenuItemLink href="/list/entry-balance">
-                <MdArrowUpward/>
-                Entradas
-            </MenuItemLink>
-            <MenuItemLink href="/list/exit-balance">
-                <MdArrowDownward/>
-                Saídas
-            </MenuItemLink>
-            <MenuItemLink href="#">
-                <MdExitToApp/>
-                Sair
-            </MenuItemLink>
-        </MenuContainer>
-    </Container>
+import {useAuth} from "../../hooks/auth";
 
-)
+import {Container, Header, LogImg, MenuContainer, MenuItemLink, MenuItemButton, Title} from "./styles";
+
+const Aside: React.FC = () => {
+    const {signOut} = useAuth();
+    return (
+        <Container>
+            <Header>
+                <LogImg src={logoImg} alt="Logo Minha Carteira"/>
+                <Title>Minha Carteira</Title>
+            </Header>
+            <MenuContainer>
+                <MenuItemLink href="/">
+                    <MdDashboard/>
+                    Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="/list/entry-balance">
+                    <MdArrowUpward/>
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="/list/exit-balance">
+                    <MdArrowDownward/>
+                    Saídas
+                </MenuItemLink>
+                <MenuItemButton onClick={signOut}>
+                    <MdExitToApp/>
+                    Sair
+                </MenuItemButton>
+            </MenuContainer>
+        </Container>
+    )
+}
 export default Aside;
